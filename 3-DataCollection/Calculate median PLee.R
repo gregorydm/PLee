996dd66e3f44ad15a7522a69b65f1ae67f85c282
Gregory De Meyer <gregory.demeyer@student.uantwerpen.be>
2022-03-07 11:25:39 +0100

    Update cfr. manuscript revision

--- 3-DataCollection/Calculate median PLee.R
+++ 3-DataCollection/Calculate median PLee.R
@@ -1,86 +1,67 @@
 # Load the tidyverse package
 require(tidyverse)
-
-# Import CRF data----
-ptlist <- read.csv("3-DataCollection/3-Data/CRF.csv")
   
-# Build  function ----
-medianPLeeperPEEPlevel <- function(id, datastart) {
+# Define  function ----
+medianPLee <- function(id, start = 0, setPEEP) {
   
-# Import signals ----
-  
-  # Get the file path
-  signalsfile <- list.files(path = paste0("3-DataCollection/3-Data/", as.character(id)),
-                            pattern = "*Signals.txt", full.names = T)
-  
-  # Import
-  signals <- read_tsv(signalsfile, 
-                      col_types = cols(X10 = col_skip()),
-                      col_names = FALSE,
-                      locale = locale(decimal_mark = ",", grouping_mark = "."),
-                      skip = 7)
-  
-  # Set column names
-  colnames(signals) <- (c("Time", "Flow", "Volume", "Paw", "Pes", "PL", "Pga", "Ptdiaf", "CO2"))
-  
-# Filter signals ----
-  # Remove all data recorded before the actual start of the experiment
-  signals <- signals %>%
-    filter(Time > datastart)
-  
-# Median PLee @ PEEP 15 ----
-  PLee_PEEP15 <- signals %>%
-    # PEEP 15 = Paw around 15 & flow around zero (i.e. end-expiratory)
-    filter(Paw < 16 & Paw > 14 & Flow < 10 & Flow > -10) %>% 
-    # Remove outliers using Tukey's rule
-    filter(PL > quantile(PL)[2] - 1.5 * IQR(PL) & PL < quantile(PL)[4] + 1.5 * IQR(PL)) %>%
-    # Calculate median
-    summarise(medianPLee = median(PL)) %>%
-    # Add PEEPlevel column
-    mutate(PEEPlevel = factor("15", levels = c("15", "10", "5"), ordered = T))
-  
-  # Median PLee @ PEEP 10 ----
-  PLee_PEEP10 <- signals %>%
-    # PEEP 10 = Paw around 10 & flow around zero (i.e. end-expiratory)
-    filter(Paw < 11 & Paw > 9 & Flow < 10 & Flow > -10) %>% 
-    # Remove outliers using Tukey's rule
-    filter(PL > quantile(PL)[2] - 1.5 * IQR(PL) & PL < quantile(PL)[4] + 1.5 * IQR(PL)) %>%
-    # Calculate median
-    summarise(medianPLee = median(PL)) %>%
-    # Add PEEPlevel column
-    mutate(PEEPlevel = factor("10", levels = c("15", "10", "5"), ordered = T))
-  
-  # Median PLee @ PEEP 5 ----
-  PLee_PEEP5 <- signals %>%
-    # PEEP 5 = Paw around 5 & flow around zero (i.e. end-expiratory)
-    filter(Paw < 6 & Paw > 4 & Flow < 10 & Flow > -10) %>% 
-    # Remove outliers using Tukey's rule
-    filter(PL > quantile(PL)[2] - 1.5 * IQR(PL) & PL < quantile(PL)[4] + 1.5 * IQR(PL)) %>%
-    # Calculate median
-    summarise(medianPLee = median(PL)) %>%
-    # Add PEEPlevel column
-    mutate(PEEPlevel = factor("5", levels = c("15", "10", "5"), ordered = T))
-
-  # Build output table
-  mediansignals <-
-    # Combine the calculations
-    rbind(PLee_PEEP15, PLee_PEEP10, PLee_PEEP5) %>%
-    # Add a column with the participants id
-    mutate(id = as.factor(id)) %>%
-    # Change order of the columns
-    select(id, PEEPlevel, medianPLee)
-  
-  return(mediansignals)
+  # Check inputs and set error messages ----
+    if (is.character(id) == F) {stop("'id' must be a character string")}
+    if (is.numeric(start) == F) stop("'start' must be a number in seconds (default: 0 seconds)")
+    if (start < 0) stop("'start' must be ≥ 0")
+    if (is.numeric(setPEEP) == F) stop("'set PEEP' must be a numeric between 0 and 30")
+    if (setPEEP < 0 | setPEEP > 30) stop("'set PEEP' must be a numeric between 0 and 30")
+    
+  # Import signals ----
+    
+    # Get the file path
+    signalsfile <- list.files(path = paste0("3-DataCollection/3-Data/", as.character(id)),
+                              pattern = "*Signals.txt", full.names = T)
+    
+    # Import
+    signals <- read_tsv(signalsfile, 
+                        col_types = cols(X10 = col_skip()),
+                        col_names = FALSE,
+                        locale = locale(decimal_mark = ",", grouping_mark = "."),
+                        skip = 7)
+    
+    # Set column names
+    colnames(signals) <- (c("Time", "Flow", "Volume", "Paw", "Pes", "PL", "Pga", "Ptdiaf", "CO2"))
+    
+  # Filter signals ----
+    # Remove all data recorded before "start"
+    signals <- signals %>%
+      filter(Time > start)
+    
+  # Median PLee ----
+    PLee <- signals %>%
+      # Paw around setPEEP & flow around zero (i.e. end-expiratory)
+      filter(Paw < setPEEP + 1 & Paw > setPEEP - 1 & Flow < 10 & Flow > -10) %>% 
+      # Remove outliers using Tukey's rule
+      filter(PL > quantile(PL)[2] - 1.5 * IQR(PL) & PL < quantile(PL)[4] + 1.5 * IQR(PL)) %>%
+      # Calculate median
+      summarise(medianPLee = median(PL)) %>%
+      # Add setPEEP column
+      mutate(setPEEP = setPEEP) %>%
+      # Add id column
+      mutate(id = id) %>%
+      # Change order of the columns
+      select(id, setPEEP, medianPLee)
+    
+    return(PLee)
 }
+
+# Import CRF data ----
+# = input data for the function
+ptlist <- read.csv("3-DataCollection/3-Data/CRF.csv")
 
 # Apply function ----
 
 # Initiate empty table
 db <- NULL
 
-# Apply function for each id in the ptlist table
+# Apply function for each id+setPEEP combination (row) in the ptlist table
 for (i in (1:nrow(ptlist))) {
-  y <- medianPLeeperPEEPlevel(id = ptlist[i,1], datastart = ptlist[i,2])
+  y <- medianPLee(id = ptlist[i,1], start = ptlist[i,2], setPEEP = ptlist[i,3])
   db <- rbind(db, y)
 }
 
@@ -89,4 +70,3 @@ rm(i, y)
 
 # Show results ----
 print(db)
-
